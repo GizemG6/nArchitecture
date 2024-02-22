bc473e6e9299b65c6f1ee6ccaa33086e79fc9842
cang8 <0.gizemgunes@gmail.com>
2024-02-22 19:32:29 +0300

    Add project files.

---
+++ Core.Application/Pipelines/Authorization/AuthorizationBehavior.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+
+namespace Core.Application.Pipelines.Authorization
+{
+    public class AuthorizationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>, ISecuredRequest
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public AuthorizationBehavior(IHttpContextAccessor httpContextAccessor) 
+        { 
+            _httpContextAccessor = httpContextAccessor;
+        }
+        public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+            //add core.security.extensions
+            //add core.crosscuttingconcerns.exceptions
+        }
+    }
+}
