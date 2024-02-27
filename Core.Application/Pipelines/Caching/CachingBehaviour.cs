39b4954f827a1731ef994576c8440e8e0d6bc6f7
cang8 <0.gizemgunes@gmail.com>
2024-02-27 22:28:15 +0300

    get-error

---
+++ Core.Application/Pipelines/Caching/CachingBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Core.Application.Pipelines.Caching
+{
+    public class CachingBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>, ICachableRequest
+    {
+        private readonly IDistributedCache _cache;
+        private readonly ILogger<CachingBehaviour<TRequest, TResponse>> _logger;
+
+        private readonly CacheSettings _cacheSettings;
+
+        public CachingBehaviour(IDistributedCache cache, ILogger<CachingBehaviour<TRequest, TResponse>> logger, IConfiguration configuration)
+        {
+            _cache = cache;
+            _logger = logger;
+            _cacheSettings = configuration.GetSection("CacheSettings").Get<CacheSettings>();
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            TResponse response;
+            
+        }
+    }
+}
